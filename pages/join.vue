4b4ed8e9b307782e634d397ab75092b2b36b9d18
Luiz Figueiredo <lffigueiredo@gmail.com>
2018-12-13 12:05:14 +0900

    components

---
+++ pages/join.vue
@@ -0,0 +1,123 @@
+<template>
+
+<div class="container">
+
+    <form-wizard @on-complete="onComplete" 
+                    title="Join Now"
+                    subtitle="Sub-title line"
+                    shape="square"
+                    color="#3498db">
+        <tab-content title="Personal details"
+                        icon="ti-user"
+                        :before-change="validateFirstTab">
+            My first tab content
+            <vue-form-generator :model="model"
+                    :schema="firstTabSchema"
+                    :options="formOptions"
+                    ref="firstTabForm">
+            </vue-form-generator>
+        </tab-content>
+        <tab-content title="Additional Info"
+                        icon="ti-settings">
+            My second tab content
+        </tab-content>
+        <tab-content title="Last step"
+                        icon="ti-check">
+            Yuhuuu! This seems pretty damn simple
+        </tab-content>
+    </form-wizard>
+ </div>
+
+
+</template>
+<script>
+import {FormWizard, TabContent} from 'vue-form-wizard'
+import VueFormGenerator from "vue-form-generator";
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
+export default {
+    components: {
+        "vue-form-generator": VueFormGenerator.component,
+        FormWizard,
+        TabContent
+    },
+    data() {
+        return {
+            model: {
+                name: "",
+                contributionPlan: "",
+                postMethod: "",
+                followersNumber: "",
+                numberPeople: "",
+                peopleInGroup: "",
+                valuePerPerson: "",
+                proposedHeading: "",
+                caseDescription: "",
+                product: "",
+                thumbnailImage: "",
+                referenceImage: "",
+                postingNotes: "",
+                specifiedUrl: "",
+                shootingInstruction: "",
+                shootingSample: "",
+                hashtag: "",
+                hashtagSetting: "",
+                nationality: "",
+                minimumAge: "",
+                maximumAge: "",
+                specificGender: "",
+                area: "",
+                recruitmentStartDate: "",
+                recruitmentEndDate: "",
+                selectionPeriodDate: "",
+                experienceStartDate: "",
+                supplementExperienceDate: "",
+                snsStartDate: "",
+                snsEndDate: "",
+            },
+            formOptions: {
+                validationErrorClass: "has-error"
+            },
+            firstTabSchema: {
+                fields: [
+                    {
+                        type: "input",
+                        inputType: "text",
+                        label: "Name",
+                        model: "name",
+                        required: true,
+                        validator: VueFormGenerator.validators.string,
+                        styleClasses: "",
+                    },
+                    {
+                        type: "input",
+                        inputType: "text",
+                        label: "E-mail",
+                        model: "email",
+                        required: true,
+                        validator: VueFormGenerator.validators.email,
+                        styleClasses: "",
+                    },
+                    {
+                    type: "radios",
+                    label: "投稿方法",
+                    model: "postMethod",
+                    required: true,
+                    values: [ "通常", "ストーリー", "カルーセル"],
+                    validator: VueFormGenerator.validators.required,
+                    styleClasses: "form-radio",
+                    },
+                ],
+            }
+        }
+    },
+    methods: {
+        onComplete: function(){
+            alert('Yay. Done!');
+        },
+        validateFirstTab() {
+            return this.$refs.firstTabForm.validate();
+        },
+    }
+}
+</script>
---
+++ plugins/components.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import Slides from '~/components/Slides.vue'
+import PortfolioCard from '~/components/PortfolioCard.vue'
+import FeatureCard from '~/components/FeatureCard.vue'
+import FeatureBlock from '~/components/FeatureBlock.vue'
+import CallToAction from '~/components/CallToAction.vue'
+
+Vue.component('c-slides', Slides)
+Vue.component('c-portfolio-card', PortfolioCard)
+Vue.component('c-feature-card', FeatureCard)
+Vue.component('c-feature-block', FeatureBlock)
+Vue.component('c-call-to-action', CallToAction)
